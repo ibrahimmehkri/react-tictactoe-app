3f14cb15776b797f19e607ac7f359d1b5b9136fd
Ibrahim Mehkri <ibrahimmehkri@gmail.com>
2018-08-14 00:29:19 +0200

    dependencies

--- index.js
+++
@@ -1,4 +0,0 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-
-ReactDOM.render(React.createElement("p", null, "Hello World"), document.getElementById("app")); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,32 @@
 module.exports = {
-  entry: "./index.js",
+  entry: "./index.jsx",
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      },
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        },
+        test: /\.jsx$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      }
+    ]
+  },
   output: {
     filename: "bundle.js",
     path: __dirname
